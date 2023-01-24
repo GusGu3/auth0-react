1565b978db4b4bd4eb9b25e8c15f92b9aad58516
GusGu3 <38086684+GusGu3@users.noreply.github.com>
2023-01-24 20:19:57 +0000

    update

--- Sample-01/src/index.js
+++ Sample-01/src/index.js
@@ -22,7 +22,7 @@ const providerConfig = {
   clientId: config.clientId,
   onRedirectCallback,
   authorizationParams: {
-    redirect_uri: window.location.origin,
+    redirect_uri: window.location.href,
     ...(config.audience ? { audience: config.audience } : null),
   },
 };
